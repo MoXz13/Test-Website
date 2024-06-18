702222f9b68f86b5e45b82a6bf5647f03059fb09
MoXz13 <165773636+MoXz13@users.noreply.github.com>
2024-06-18 09:16:30 +0200

    Update login.js

--- login.js
+++ login.js
@@ -1,14 +1,15 @@
 document.getElementById("login-form").addEventListener("submit", function(event) {
-    event.preventDefault(); // Prevent form submission
-  
-    var username = document.getElementById("username").value;
-    var password = document.getElementById("password").value;
-  
-    if (username === "Altin" && password === "Altin") {
-      window.location.href = "dashboard.html";
-    } else {
-      alert("Falscher Benutzername oder Passwort. Bitte versuchen Sie es erneut.");
-    }
-  });
-  
-  
+  event.preventDefault(); // Prevent form submission
+
+  var username = document.getElementById("username").value;
+  var password = document.getElementById("password").value;
+
+  // Simulate authentication
+  if (username === "Moritz" && password === "Altin") {
+    // Save login state in localStorage
+    localStorage.setItem("authenticated", "true");
+    window.location.href = "dashboard.html";
+  } else {
+    alert("Falscher Benutzername oder Passwort. Bitte versuchen Sie es erneut.");
+  }
+});
